c4036309247b983534e412f90ff3e7015f0e8389
Shayan Khan <shayanjob20@gmail.com>
2019-03-08 15:29:42 +0500

    add Pagination in datatable

--- public/js/app.js
+++ public/js/app.js
@@ -104654,7 +104654,7 @@ __webpack_require__(/*! ./bootstrap */ "./resources/js/bootstrap.js");
  */
 
 
-__webpack_require__(/*! ./components/Example */ "./resources/js/components/Example.js");
+__webpack_require__(/*! ./components/dataTable */ "./resources/js/components/dataTable.js");
 
 /***/ }),
 
@@ -104716,179 +104716,6 @@ if (token) {
 
 /***/ }),
 
-/***/ "./resources/js/components/Example.js":
-/*!********************************************!*\
-  !*** ./resources/js/components/Example.js ***!
-  \********************************************/
-/*! exports provided: default */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
-
-"use strict";
-__webpack_require__.r(__webpack_exports__);
-/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "default", function() { return Example; });
-/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
-/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
-/* harmony import */ var react_dom__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react-dom */ "./node_modules/react-dom/index.js");
-/* harmony import */ var react_dom__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(react_dom__WEBPACK_IMPORTED_MODULE_1__);
-/* harmony import */ var _Webix__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./Webix */ "./resources/js/components/Webix.js");
-/* harmony import */ var axios__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! axios */ "./node_modules/axios/index.js");
-/* harmony import */ var axios__WEBPACK_IMPORTED_MODULE_3___default = /*#__PURE__*/__webpack_require__.n(axios__WEBPACK_IMPORTED_MODULE_3__);
-function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
-
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
-
-function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
-
-function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
-
-function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
-
-function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
-
-function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
-
-function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
-
-
-
-
-
-
-function getUI(select) {
-  return {
-    view: "datatable",
-    // pager: {
-    //     container: "paging_here",
-    //     size: 10,
-    //     group: 10
-    // },
-    scroll: true,
-    width: 900,
-    // autoheight: true,
-    height: 500,
-    select: true,
-    columns: [{
-      id: "ref",
-      fillspace: 2,
-      sort: "int"
-    }, {
-      id: "title",
-      header: ["Title", {
-        content: "textFilter"
-      }],
-      fillspace: 4,
-      sort: "string",
-      filter: "string"
-    }, {
-      id: "team",
-      header: ["Team", {
-        content: "textFilter"
-      }],
-      fillspace: 4,
-      sort: "string"
-    }, {
-      id: "client",
-      header: ["Client", {
-        content: "textFilter"
-      }],
-      fillspace: 3,
-      sort: "string"
-    }, {
-      id: "pm",
-      header: ["PM", {
-        content: "textFilter"
-      }],
-      fillspace: 2
-    }, {
-      id: "status",
-      header: ["Status", {
-        content: "textFilter"
-      }],
-      fillspace: 2
-    }, {
-      id: "deadline",
-      header: ["Status", {
-        content: "textFilter"
-      }],
-      fillspace: 3
-    }, {
-      id: "date Sent",
-      header: ["Status", {
-        content: "textFilter"
-      }],
-      fillspace: 3
-    }] // on: {
-    //     onAfterSelect: function(id) {
-    //         select(id);
-    //     }
-    // }
-
-  };
-}
-
-var Example =
-/*#__PURE__*/
-function (_Component) {
-  _inherits(Example, _Component);
-
-  function Example(props) {
-    var _this;
-
-    _classCallCheck(this, Example);
-
-    _this = _possibleConstructorReturn(this, _getPrototypeOf(Example).call(this, props));
-    _this.state = {
-      data: []
-    };
-    return _this;
-  }
-
-  _createClass(Example, [{
-    key: "componentDidMount",
-    value: function componentDidMount() {
-      var _this2 = this;
-
-      axios__WEBPACK_IMPORTED_MODULE_3___default.a.get("http://localhost/x-tracking/public/data.json").then(function (resp) {
-        var getShowData = resp.data.map(function (item) {
-          return {
-            ref: item.ref,
-            title: item.title,
-            team: item.team,
-            client: item.client,
-            pm: item.pm,
-            status: item.status,
-            deadline: item.deadline
-          };
-        });
-
-        _this2.setState({
-          data: getShowData
-        });
-      });
-    }
-  }, {
-    key: "render",
-    value: function render() {
-      return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_Webix__WEBPACK_IMPORTED_MODULE_2__["default"], {
-        ui: getUI(),
-        data: this.state.data
-      });
-    }
-  }]);
-
-  return Example;
-}(react__WEBPACK_IMPORTED_MODULE_0__["Component"]);
-
-
-
-if (document.getElementById("app")) {
-  react_dom__WEBPACK_IMPORTED_MODULE_1___default.a.render(react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(Example, null), document.getElementById("app"));
-}
-
-/***/ }),
-
 /***/ "./resources/js/components/Webix.js":
 /*!******************************************!*\
   !*** ./resources/js/components/Webix.js ***!
@@ -104945,9 +104772,12 @@ function (_Component) {
   _createClass(Webix, [{
     key: "render",
     value: function render() {
-      return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+      return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", null, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+        id: "box",
         ref: "root"
-      });
+      }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+        id: "paging_wrapper"
+      }));
     }
   }, {
     key: "setWebixData",
@@ -104979,6 +104809,191 @@ function (_Component) {
 }(react__WEBPACK_IMPORTED_MODULE_0__["Component"]);
 
 /* harmony default export */ __webpack_exports__["default"] = (Webix);
+
+/***/ }),
+
+/***/ "./resources/js/components/dataTable.js":
+/*!**********************************************!*\
+  !*** ./resources/js/components/dataTable.js ***!
+  \**********************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "default", function() { return Example; });
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var react_dom__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react-dom */ "./node_modules/react-dom/index.js");
+/* harmony import */ var react_dom__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(react_dom__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var _Webix__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./Webix */ "./resources/js/components/Webix.js");
+/* harmony import */ var axios__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! axios */ "./node_modules/axios/index.js");
+/* harmony import */ var axios__WEBPACK_IMPORTED_MODULE_3___default = /*#__PURE__*/__webpack_require__.n(axios__WEBPACK_IMPORTED_MODULE_3__);
+function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
+
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+
+function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
+
+function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
+
+
+
+
+
+
+function getUI(select) {
+  return {
+    view: "datatable",
+    container: "box",
+    pager: {
+      template: "{common.first()} {common.prev()} {common.pages()} \n            {common.next()} {common.last()}",
+      container: "paging_wrapper",
+      size: 20,
+      group: 5
+    },
+    scroll: true,
+    height: 400,
+    select: true,
+    columns: [{
+      id: "ch1",
+      header: "",
+      template: "{common.checkbox()}",
+      width: 80
+    }, {
+      id: "ref",
+      width: 80,
+      header: ["Ref", {
+        content: "textFilter"
+      }],
+      sort: "int"
+    }, {
+      id: "title",
+      header: ["Title", {
+        content: "textFilter"
+      }],
+      width: 300,
+      sort: "string",
+      filter: "string"
+    }, {
+      id: "team",
+      header: ["Team", {
+        content: "textFilter"
+      }],
+      width: 200,
+      sort: "string"
+    }, {
+      id: "client",
+      header: ["Client", {
+        content: "textFilter"
+      }],
+      width: 200,
+      sort: "string"
+    }, {
+      id: "pm",
+      header: ["PM", {
+        content: "textFilter"
+      }],
+      width: 100,
+      sort: "string"
+    }, {
+      id: "status",
+      header: ["Status", {
+        content: "textFilter"
+      }],
+      width: 100,
+      sort: "string"
+    }, {
+      id: "deadline",
+      header: ["Deadline", {
+        content: "textFilter"
+      }],
+      width: 100,
+      sort: "string"
+    }, {
+      id: "date Sent",
+      header: ["Date Sent", {
+        content: "textFilter"
+      }],
+      width: 150,
+      sort: "string"
+    }] // on: {
+    //     onAfterSelect: function(id) {
+    //         select(id);
+    //     }
+    // }
+
+  };
+}
+
+var Example =
+/*#__PURE__*/
+function (_Component) {
+  _inherits(Example, _Component);
+
+  function Example(props) {
+    var _this;
+
+    _classCallCheck(this, Example);
+
+    _this = _possibleConstructorReturn(this, _getPrototypeOf(Example).call(this, props));
+    _this.state = {
+      data: []
+    };
+    return _this;
+  }
+
+  _createClass(Example, [{
+    key: "componentDidMount",
+    value: function componentDidMount() {
+      var _this2 = this;
+
+      axios__WEBPACK_IMPORTED_MODULE_3___default.a.get("../data.json").then(function (resp) {
+        var getShowData = resp.data.map(function (item) {
+          return {
+            ref: item.ref,
+            title: item.title,
+            team: item.team,
+            client: item.client,
+            pm: item.pm,
+            status: item.status,
+            deadline: item.deadline
+          };
+        });
+
+        _this2.setState({
+          data: getShowData
+        });
+      });
+    }
+  }, {
+    key: "render",
+    value: function render() {
+      return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_Webix__WEBPACK_IMPORTED_MODULE_2__["default"], {
+        ui: getUI(),
+        data: this.state.data
+      });
+    }
+  }]);
+
+  return Example;
+}(react__WEBPACK_IMPORTED_MODULE_0__["Component"]);
+
+
+
+if (document.getElementById("app")) {
+  react_dom__WEBPACK_IMPORTED_MODULE_1___default.a.render(react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(Example, null), document.getElementById("app"));
+}
 
 /***/ }),
 
--- resources/js/components/webix.js
+++ resources/js/components/webix.js
@@ -1,50 +1,47 @@
 // example of custom component with Webix UI inside
 // this one is a static view, not linked to the React data store
 
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
 
-import 'webix/webix.js';
-import 'webix/webix.css';
+import "webix/webix.js";
+import "webix/webix.css";
 
 class Webix extends Component {
-  render() {
-    return (
-      <div ref="root"></div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <div id="box" ref="root" />
+                <div id="paging_wrapper" />
+            </div>
+        );
+    }
 
-  setWebixData(data){
-    const ui = this.ui;
-    if (ui.setValues)
-      ui.setValues(data);
-    else if (ui.parse)
-      ui.parse(data)
-    else if (ui.setValue)
-      ui.setValue(data); 
-  }
+    setWebixData(data) {
+        const ui = this.ui;
+        if (ui.setValues) ui.setValues(data);
+        else if (ui.parse) ui.parse(data);
+        else if (ui.setValue) ui.setValue(data);
+    }
 
-  componentWillUnmount(){
-    this.ui.destructor();
-    this.ui = null;
-  }
+    componentWillUnmount() {
+        this.ui.destructor();
+        this.ui = null;
+    }
 
-  componentWillUpdate(props){
-    if (props.data)
-      this.setWebixData(props.data);
-    if (props.select)
-      this.select(props.select);
-  }
+    componentWillUpdate(props) {
+        if (props.data) this.setWebixData(props.data);
+        if (props.select) this.select(props.select);
+    }
 
-  componentDidMount(){
-  	this.ui = window.webix.ui(
-  	  this.props.ui, 
-  	  ReactDOM.findDOMNode(this.refs.root)
-	  );
+    componentDidMount() {
+        this.ui = window.webix.ui(
+            this.props.ui,
+            ReactDOM.findDOMNode(this.refs.root)
+        );
 
-    this.componentWillUpdate(this.props);
-  }
-  
+        this.componentWillUpdate(this.props);
+    }
 }
 
 export default Webix;
